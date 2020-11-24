2aeaa8e9310bfe0d031107805dbe2c748ac1d56c
austinooten <43047326+austinooten@users.noreply.github.com>
2020-11-24 12:14:54 -0500

    Update wp-login.php

--- wp-login.php
+++ wp-login.php
@@ -7,7 +7,7 @@
  *
  * @package WordPress
  */
-
+//Authors Austin Ooten, Anthony Lowery
 /** Make sure that the WordPress bootstrap has run before continuing. */
 require __DIR__ . '/wp-load.php';
 
